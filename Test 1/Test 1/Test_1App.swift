5ec75cd0419653b35ae836fbcbf4f1f91c3b0f80
Christiane <Christiane>
2025-03-29 12:23:56 +0100

    foregroundstyle red

---
+++ Test 1/Test 1/Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_1App.swift
+//  Test 1
+//
+//  Created by Christiane Roth on 29.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
